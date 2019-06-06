848840b4d4a990b00a54d7caf3974fa927a483c5
Kenton Bocock <kentonbo@gmail.com>
2019-06-05 21:48:02 -0400

    fix stdout pipe to terminal. Add cli info chk

--- src/package/angular-update.ts
+++ src/package/angular-update.ts
@@ -1,19 +1,42 @@
 const execa = require('execa');
 import * as vscode from 'vscode';
 const NG_CMD = 'ng';
+const CLI_CHK_CMD = 'npm info @angular/cli';
 const workspace = vscode.workspace.workspaceFolders![0];
 
 export async function updateNg(next: boolean = false) {
   const updateAll = 'ng update --all';
   const terminal = vscode.window.createTerminal(`Angular Evergreen`);
+  let latest = '';
+  let vnext = '';
 
   let updateCMD = next ? updateAll + ' --next' : updateAll;
   terminal.show();
-  terminal.sendText(updateCMD);
+
   try {
-    let stdout = await execa.shell(updateCMD, { cwd: workspace.uri.path });
-    terminal.sendText(stdout);
+    const clistdout = await execa.shell(CLI_CHK_CMD, { cwd: workspace.uri.path });
+    latest = getVersionsFromStdout(clistdout.stdout, 'latest');
+    vnext = getVersionsFromStdout(clistdout.stdout, 'next');
+    const ngstdout = await execa.shell(updateCMD, { cwd: workspace.uri.path });
+    const message = ngstdout.stdout.message.replace('//n', '');
+    terminal.sendText(message);
   } catch (error) {
-    terminal.sendText(error);
+    const message = error.message.replace('//n', '');
+    terminal.sendText(message);
   }
+
+  // todo allow user to choose version to update.
+  vscode.window.showInformationMessage(
+    `Available @Angular Cli Version: Latest: ${latest} Next: ${vnext}`
+  );
+}
+
+function getVersionsFromStdout(stdout: string, versionType: string) {
+  const length = 25;
+  const sIndex = stdout.indexOf(versionType);
+
+  const roughStr = stdout.substr(sIndex, length);
+  const colStr = roughStr.substr(roughStr.indexOf(':') + 1, 6);
+
+  return colStr.trim();
 }
