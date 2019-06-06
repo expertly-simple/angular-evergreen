d6adf49f787d576b33135cfaac2a140bac7e5521
Kenton Bocock <kentonbo@gmail.com>
2019-06-05 22:10:40 -0400

    add terminal renderer for stdout

--- src/package/angular-update.ts
+++ src/package/angular-update.ts
@@ -6,23 +6,20 @@ const workspace = vscode.workspace.workspaceFolders![0];
 
 export async function updateNg(next: boolean = false) {
   const updateAll = 'ng update --all';
-  const terminal = vscode.window.createTerminal(`Angular Evergreen`);
   let latest = '';
   let vnext = '';
 
   let updateCMD = next ? updateAll + ' --next' : updateAll;
-  terminal.show();
-
+  const render = (<any>vscode.window).createTerminalRenderer('Angular Evergreen');
+  render.terminal.show();
   try {
     const clistdout = await execa.shell(CLI_CHK_CMD, { cwd: workspace.uri.path });
     latest = getVersionsFromStdout(clistdout.stdout, 'latest');
     vnext = getVersionsFromStdout(clistdout.stdout, 'next');
     const ngstdout = await execa.shell(updateCMD, { cwd: workspace.uri.path });
-    const message = ngstdout.stdout.message.replace('//n', '');
-    terminal.sendText(message);
+    render.write(ngstdout.stdout.message);
   } catch (error) {
-    const message = error.message.replace('//n', '');
-    terminal.sendText(message);
+    render.write(error.message);
   }
 
   // todo allow user to choose version to update.
