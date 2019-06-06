3726e9e81d30d1f3ca92be449fb60e5daef15489
Kenton Bocock <kentonbo@gmail.com>
2019-06-06 19:43:57 -0400

    more formatting. Set terminal txt green

--- src/package/angular-update.ts
+++ src/package/angular-update.ts
@@ -1,38 +1,38 @@
 const execa = require('execa');
 import * as vscode from 'vscode';
-const NG_CMD = 'ng';
 const CLI_CHK_CMD = 'npm info @angular/cli';
+const NG_ALL_CMD = 'ng update --all';
 const workspace = vscode.workspace.workspaceFolders![0];
 
 export async function updateNg(next: boolean = false) {
-  const updateAll = 'ng update --all';
   let latest = '';
   let vnext = '';
 
-  let updateCMD = next ? updateAll + ' --next' : updateAll;
+  let updateCMD = next ? NG_ALL_CMD + ' --next' : NG_ALL_CMD;
   const render = (<any>vscode.window).createTerminalRenderer('Angular Evergreen');
   render.terminal.show();
-  render.write('~~~~ Welcome to Angular Evergreen ~~~~ \r\n\n todo: more info here?');
+  render.write('\x1b[32m ~~~~ Welcome to Angular Evergreen ~~~~ \r\n\n');
   try {
+    // get latest cli version
     const clistdout = await execa.shell(CLI_CHK_CMD, { cwd: workspace.uri.path });
     latest = getVersionsFromStdout(clistdout.stdout, 'latest');
     vnext = getVersionsFromStdout(clistdout.stdout, 'next');
+    render.write(
+      `Available @Angular Cli Version: Latest: ${latest} Next: ${vnext} \r\n\n`
+    );
+    // todo: prompt just cli update here
+    // try updatet
     const ngstdout = await execa.shell(updateCMD, { cwd: workspace.uri.path });
-    render.write(ngstdout.stdout.message.replace('\n', '\r\n\n'));
+    render.write(ngstdout.stdout.message.replace(/[\n\r]/g, ' ').replace('   ', ''));
   } catch (error) {
-    render.write(error.message.replace('\n', '\r\n\n'));
+    let msg = error.message.replace(/[\n\r]/g, ' ').replace('   ', '');
+    render.write(msg);
   }
-
-  // todo allow user to choose version to update.
-  vscode.window.showInformationMessage(
-    `Available @Angular Cli Version: Latest: ${latest} Next: ${vnext}`
-  );
 }
 
 function getVersionsFromStdout(stdout: string, versionType: string) {
   const length = 25;
   const sIndex = stdout.indexOf(versionType);
-
   const roughStr = stdout.substr(sIndex, length);
   const colStr = roughStr.substr(roughStr.indexOf(':') + 1, 6);
 
