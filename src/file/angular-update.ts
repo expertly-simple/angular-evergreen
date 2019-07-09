56dae1d6898baa0adea0c3dc5cacda76ccf58895
Kenton Bocock <kentonbo@gmail.com>
2019-07-08 20:03:04 -0400

    remove quiet move angular func

--- src/file/angular-update.ts
+++ src/file/angular-update.ts
@@ -1,13 +1,25 @@
 import * as execa from 'execa'
 import * as vscode from 'vscode'
+import { isGitClean } from './git-manager'
 
-const CLI_CHK_CMD = 'npm info @angular/cli'
 const NG_ALL_CMD = 'ng update --all '
 const workspace = vscode.workspace.workspaceFolders![0]
 
 export enum UpdateArgs {
   next = '--next',
   force = '--force',
+}
+
+export async function tryAngularUpdate(updateArg: UpdateArgs[]) {
+  let gitClean = await isGitClean()
+  if (gitClean) {
+    let message = 'Update completed! Project is evergreen 🌲'
+    const status = await ngUpdate(updateArg)
+  } else {
+    vscode.window.showErrorMessage(
+      "Can't update: You should ensure git branch is clean & up-to-date"
+    )
+  }
 }
 
 export async function ngUpdate(args?: UpdateArgs[]): Promise<boolean> {
@@ -25,6 +37,7 @@ export async function ngUpdate(args?: UpdateArgs[]): Promise<boolean> {
     render.write(`\r\n ${msg} \r\n`)
     // check if user wants to force
     forceUpdate(render, `${updateCMD} ${UpdateArgs.force}`)
+
     return false
   }
 }
