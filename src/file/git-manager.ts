8cc66be2ffdea0d5f0acc468da3f6a97853fa51e
brendoncaulkins <brendon.caulkins@excella.com>
2019-10-19 18:15:50 -0400

    Add new terminal-less way to interact with GIT

--- src/file/git-manager.ts
+++ src/file/git-manager.ts
@@ -1,28 +1,29 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
-export async function isGitClean(): Promise<Boolean> {
-  const terminal = vscode.window.createTerminal(`Angular Evergreen`);
-  terminal.show();
-  terminal.sendText('git status');
-  let output = '';
-  let datawatcher = (<any>terminal).onDidWriteData((data: string) => {
-    output += data;
-  });
+import { API, GitExtension } from '../types/git'
 
-  var promise = new Promise<Boolean>(function(resolve) {
-    setTimeout(() => {
-      if (
-        output.includes('Changes') ||
-        output.includes('Untracked') ||
-        output.includes('ahead') ||
-        output.includes('behind')
-      ) {
-        resolve(false);
-      } else {
-        resolve(true);
-      }
-      terminal.dispose();
-    }, 1000);
-  });
-  return promise;
+function getGitApi(): API | null {
+  const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')
+  if (!gitExtension) {
+    console.log("Unable to connect to VSCode's GIT SCM Extension.  Is it disabled?")
+    return null
+  }
+
+  return gitExtension.exports.getAPI(1)
+}
+
+export function onCleanGitBranch(): boolean {
+  const git = getGitApi()
+  if (!git) {
+    return false
+  }
+
+  const isClean = git.repositories
+    .map(repo => repo.state.workingTreeChanges.length === 0)
+    .reduce((flag, repoState) => {
+      return flag && repoState
+    })
+
+  console.log('AngularEvergreen::git-manager::onCleanGitBranch: ', isClean)
+  return isClean
 }
